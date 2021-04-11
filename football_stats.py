608037bb55c903f2cc397a22eb82b48eb34190a9
kozousps <kou.omori90@spsmail.cuny.edu>
2021-04-11 12:15:35 -0400

    first

--- football_stats.py
+++ football_stats.py
@@ -1,2 +1,18 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+cont = 'regular/qualifiers/?sortcol=td&sortdir=descending'
+url = 'https://www.cbssports.com/nfl/stats/player/rushing/nfl/{}'.format(cont)
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page, 'lxml')
+
+
 if __name__ == "__main__":
-    pass
+    for tr in soup.find_all('tr', class_='TableBase-bodyTr', limit=20):
+        name = tr.find('span', class_='CellPlayerName--long').a.text
+        position = tr.find('span', class_='CellPlayerName-position').text
+        team = tr.find('span', class_='CellPlayerName-team').text
+        tds = tr.find_all('td')
+        tdwn = tds[6].get_text()
+        print('{}\t{}\t{}\t{}'.format(name.strip(), position.strip(),
+                                      team.strip(), tdwn.strip()))
--- nfl_spreads.py
+++ nfl_spreads.py
@@ -1,2 +1,24 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+url = 'https://en.wikipedia.org/wiki/Shark_attack'
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page, 'lxml')
+
+table = soup.find('table', class_="wikitable sortable")
+
+
 if __name__ == "__main__":
-    pass
+    print("table for shark attacks not NFL spreads")
+
+    title = table.caption.text
+    print(title)
+
+    for tbody in table.find_all('tbody'):
+        rows = tbody.find_all('tr')[1:]
+        for row in rows:
+            region = row.find('td').text
+            total = row.find_all('td')[1].get_text()
+            fatal = row.find_all('td')[2].get_text()
+            last = row.find_all('td')[3].get_text()
+            print('{}\t{}\t{}\t{}'.format(region, total, fatal, last))
